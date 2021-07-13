38bff8470d00e7de3e40a6723aabab71fbbf45b8
FPG1993 <franciscoperezgamez@uma.es>
2021-07-13 18:18:43 +0200

    This is an Useless function

---
+++ R/prueba.R
@@ -0,0 +1,17 @@
+#' Nothing
+#'
+#' @param arg1
+#' @param arg2
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+new_funtion<- function(arg1,arg2,...) {
+
+  # This ads 1 plus 1
+  #
+  1+1
+  return(True)
+}
